bcd4fc7d82f4d20f6ca168b33931cf0a4971739b
EungGu Yun <yuneg11@gmail.com>
2021-05-28 13:06:34 +0000

    Change regression

--- experiments/regression.py
+++ experiments/regression.py
@@ -31,6 +31,15 @@ def add_subparser(subparsers):
     parser.add_argument("-s", "--seed", default=10, type=int)
 
 
+def gaussian_nll(test_y, mean, std):
+    neg_log_prob = -stats.norm.logpdf(test_y, mean, std)
+    return neg_log_prob
+
+def studentt_nll(test_y, freedom, mean, std):
+    neg_log_prob = -stats.t.logpdf(test_y, freedom, mean, std)
+    return neg_log_prob
+
+
 def main(dataset, num_hiddens, w_variance, b_variance, activation,
          alpha, beta, epsilon_log_variance, last_layer_variance, seed, **kwargs):
 
@@ -46,8 +55,8 @@ def main(dataset, num_hiddens, w_variance, b_variance, activation,
     train_x, train_y, valid_x, valid_y, test_x, test_y = splits
 
     train_num = len(train_x)
-    valid_num = len(valid_x)
-    test_num = len(test_x)
+    # valid_num = len(valid_x)
+    # test_num = len(test_x)
 
     if seed >= 0:
         train_valid_x = np.concatenate([train_x, valid_x], axis=0)
@@ -57,8 +66,8 @@ def main(dataset, num_hiddens, w_variance, b_variance, activation,
         train_x, valid_x = train_valid_x[:train_num], train_valid_x[train_num:]
         train_y, valid_y = train_valid_y[:train_num], train_valid_y[train_num:]
 
-    train_valid = jnp.concatenate([train_x, valid_x], axis=0)
-    train_test = jnp.concatenate([train_x, test_x], axis=0)
+    # train_valid = jnp.concatenate([train_x, valid_x], axis=0)
+    # train_test = jnp.concatenate([train_x, test_x], axis=0)
 
     # Models
 
@@ -93,66 +102,6 @@ def main(dataset, num_hiddens, w_variance, b_variance, activation,
     _, nngp_covariance_2_valid = predict_fn_2(x_test=valid_x, get="nngp", compute_cov=True)
     _, nngp_covariance_2_test = predict_fn_2(x_test=test_x, get="nngp", compute_cov=True)
 
-    # Valid
-
-    train_valid_nngp_kernel = kernel_fn_2(train_valid, train_valid, "nngp")
-
-    nu = 2 * alpha
-    mean = jnp.zeros(train_num + valid_num)
-    covariance = beta / alpha * train_valid_nngp_kernel
-
-    conditional_nu = nu + train_num
-
-    split_kernels = utils.split_kernel(covariance, train_num)
-    kernel_train_train, kernel_train_valid, kernel_valid_train, kernel_valid_valid = split_kernels
-
-    inverse_k_11 = jnp.linalg.inv(kernel_train_train + epsilon_variance * jnp.eye(train_num))
-    d_1 = nu + jnp.matmul(jnp.matmul(train_y.T, inverse_k_11),train_y)
-
-    posterior_kernel = nngp_covariance_2_valid
-    conditional_kernel = d_1 / conditional_nu * beta / alpha * posterior_kernel
-
-    valid_std = jnp.sqrt(jnp.diag(conditional_kernel))
-
-    # Test
-
-    train_test_nngp_kernel = kernel_fn_2(train_test, train_test, "nngp")
-
-    nu = 2 * alpha
-    mean = jnp.zeros(train_num + test_num)
-    covariance = beta / alpha * train_test_nngp_kernel
-
-    split_kernels = utils.split_kernel(covariance, train_num)
-    kernel_train_train, kernel_train_test, kernel_test_train, kernel_test_test = split_kernels
-
-    posterior_kernel = nngp_covariance_2_test
-    conditional_kernel = d_1 / conditional_nu * beta / alpha * posterior_kernel
-
-    test_std = jnp.sqrt(jnp.diag(conditional_kernel))
-
-    # make nll calculator for test points
-
-    def gaussian_nll(test_y, mean, std):
-        neg_log_prob = -stats.norm.logpdf(test_y, mean, std)
-        return neg_log_prob
-
-    def studentt_nll(test_y, freedom, mean, std):
-        neg_log_prob = -stats.t.logpdf(test_y, freedom, mean, std)
-        return neg_log_prob
-
-
-    valid_neg_log_prob_invgamma = jnp.mean(jnp.array([
-        studentt_nll(y, conditional_nu, nngp_mean, std)
-        for y, nngp_mean, std
-        in zip(valid_y, nngp_mean_valid, valid_std)
-    ]))
-
-    test_neg_log_prob_invgamma = jnp.mean(jnp.array([
-        studentt_nll(y, conditional_nu, nngp_mean, std)
-        for y, nngp_mean, std
-        in zip(test_y, nngp_mean_test, test_std)
-    ]))
-
     valid_neg_log_prob_constant = jnp.mean(jnp.array([
         gaussian_nll(y, nngp_mean, nngp_std)
         for y, nngp_mean, nngp_std
@@ -165,14 +114,59 @@ def main(dataset, num_hiddens, w_variance, b_variance, activation,
         in zip(test_y, nngp_mean_test, nngp_std_test)
     ]))
 
-    print("-------------------------------------------------------------")
-    print("num_hiddens: {:<2d}  / act:   {}".format(num_hiddens, activation))
-    print("w_variance:  {:1.1f} / alpha: {:1.1f}".format(w_variance, alpha))
-    print("b_variance:  {:1.1f} / beta:  {:1.1f}".format(b_variance, beta))
-    print("epsilon_log_variance: {:.2f}".format(raw_epsilon_log_variance))
-    print("last_layer_variance: {:.2f}        / seed: {}".format(last_layer_variance, seed))
-    print("---------------------------------------------")
-    print("Valid NLL for invgamma prior: [{:13.8f}]".format(valid_neg_log_prob_invgamma))
-    print("Valid NLL for constant prior: [{:13.8f}]".format(valid_neg_log_prob_constant))
-    print("Test NLL for invgamma prior:  [{:13.8f}]".format(test_neg_log_prob_invgamma))
-    print("Test NLL for constant prior:  [{:13.8f}]".format(test_neg_log_prob_constant))
+    # Valid
+    for alpha, beta in [
+        [0.5, 0.5], [1.0, 0.5], [1.5, 0.5], [2.0, 0.5], [4.0, 0.5], [8.0, 0.5],
+        [0.5, 1.0], [1.0, 1.0], [1.5, 1.0], [2.0, 1.0], [4.0, 1.0], [8.0, 1.0],
+        [0.5, 1.5], [1.0, 1.5], [0.5, 2.0], [1.0, 2.0], [0.5, 4.0], [1.0, 4.0],
+        [0.5, 8.0], [1.0, 8.0],
+    ]:
+        train_valid_nngp_kernel = kernel_fn_2(train_x, train_x, "nngp")
+
+        nu = 2 * alpha
+        # mean = jnp.zeros(train_num + valid_num)
+        covariance = beta / alpha * train_valid_nngp_kernel
+
+        conditional_nu = nu + train_num
+
+        kernel_train_train = covariance
+
+        inverse_k_11 = jnp.linalg.inv(kernel_train_train + epsilon_variance * jnp.eye(train_num))
+        d_1 = nu + jnp.matmul(jnp.matmul(train_y.T, inverse_k_11),train_y)
+
+        posterior_kernel = nngp_covariance_2_valid
+        conditional_kernel = d_1 / conditional_nu * beta / alpha * posterior_kernel
+
+        valid_std = jnp.sqrt(jnp.diag(conditional_kernel))
+
+        # Test
+
+        posterior_kernel = nngp_covariance_2_test
+        conditional_kernel = d_1 / conditional_nu * beta / alpha * posterior_kernel
+
+        test_std = jnp.sqrt(jnp.diag(conditional_kernel))
+
+        # make nll calculator for test points
+        valid_neg_log_prob_invgamma = jnp.mean(jnp.array([
+            studentt_nll(y, conditional_nu, nngp_mean, std)
+            for y, nngp_mean, std
+            in zip(valid_y, nngp_mean_valid, valid_std)
+        ]))
+
+        test_neg_log_prob_invgamma = jnp.mean(jnp.array([
+            studentt_nll(y, conditional_nu, nngp_mean, std)
+            for y, nngp_mean, std
+            in zip(test_y, nngp_mean_test, test_std)
+        ]))
+
+        print("-------------------------------------------------------------")
+        print("num_hiddens: {:<2d}  / act:   {}".format(num_hiddens, activation))
+        print("w_variance:  {:1.1f} / alpha: {:1.1f}".format(w_variance, alpha))
+        print("b_variance:  {:1.1f} / beta:  {:1.1f}".format(b_variance, beta))
+        print("epsilon_log_variance: {:.2f}".format(raw_epsilon_log_variance))
+        print("last_layer_variance: {:.2f}        / seed: {}".format(last_layer_variance, seed))
+        print("---------------------------------------------")
+        print("Valid NLL for invgamma prior: [{:13.8f}]".format(valid_neg_log_prob_invgamma))
+        print("Valid NLL for constant prior: [{:13.8f}]".format(valid_neg_log_prob_constant))
+        print("Test NLL for invgamma prior:  [{:13.8f}]".format(test_neg_log_prob_invgamma))
+        print("Test NLL for constant prior:  [{:13.8f}]".format(test_neg_log_prob_constant))
